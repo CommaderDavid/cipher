47d0587c7b39f0fa74187e15deb1c5fd2ea9c908
David Boedigheimer <stealth259@gmail.com>
2019-08-27 09:42:23 -0700

    Fix variable and funtion problem. Add string to array and back function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   var mainSentence = prompt("Enter your sentence.");
   console.log(mainSentence);
-
-  var firstLast = mainSentence.charAt(0).toUpperCase() + mainSentence.charAt(mainSentence.length-1).toUpperCase();
-  console.log(firstLast);
-
-  var reverse = mainSentence.charAt(mainSentence.length-1).toUpperCase() + mainSentence.charAt(0).toUpperCase();
-  console.log(reverse);
-
-  var show = (firstLast);
-  var show = (reverse);
-  alert(show);
-
+  var step1 = firstLast(mainSentence);
+  console.log(step1);
+  var step2 = reverseString(step1);
+  console.log(step2);
 });
+
+function firstLast(mainSentence){
+  return mainSentence.charAt(0).toUpperCase() + mainSentence.charAt(mainSentence.length-1).toUpperCase();
+};
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+};
