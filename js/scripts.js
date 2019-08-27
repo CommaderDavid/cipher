6e27707f0fdb5b11b930810fc521ab1704ec38be
David Boedigheimer <stealth259@gmail.com>
2019-08-27 09:12:45 -0700

    Add third function, add show function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   var mainSentence = prompt("Enter your sentence.");
   console.log(mainSentence);
-  var firstLast = mainSentence.charAt(0) + mainSentence.charAt(mainSentence.length-1);
+
+  var firstLast = mainSentence.charAt(0).toUpperCase() + mainSentence.charAt(mainSentence.length-1).toUpperCase();
   console.log(firstLast);
+
+  var reverse = mainSentence.charAt(mainSentence.length-1).toUpperCase() + mainSentence.charAt(0).toUpperCase();
+  console.log(reverse);
+
+  var show = (firstLast);
+  var show = (reverse);
+  alert(show);
+
 });
